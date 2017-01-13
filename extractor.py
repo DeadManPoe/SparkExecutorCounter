8dc80cad4f9c70bf947cf504031e4f64f6c7c428
giorgiopea <annatar93@gmail.com>
2017-01-13 15:47:23 +0100

    fixing

--- extractor.py
+++ extractor.py
@@ -1,70 +1,61 @@
-
 import json
 import csv
 import os
 import sys
 
+import re
+
+
 class Extractor:
-    def __init__(self,filename,targetDirectory):
-        self.filename = filename
-        self.file = None
-        self.targetDirectory = targetDirectory
+    def __init__(self, top_directory, target_directory):
+        self.top_directory = top_directory
+        self.csv_file = None
+        self.csv_writer = None
+        self.targetDirectory = target_directory
 
     def run(self):
-        self.openFile()
-        self.writeFile()
+        self.create_csv()
+        pattern = re.compile("application_([0-9]+)_([0-9]+)")
+        for f in os.listdir(self.top_directory):
+            if pattern.match(f) and not (f.endswith(".zip")):
+                self.writefile(f, self.extract(self.top_directory + "/" + f))
+        self.csv_file.close()
 
-    def openFile(self):
-        if os.path.exists(self.filename):
-            try:
-                self.file = open(self.filename,"r")
-            except:
-                print("Reading error")
-                exit(-1)
-        else:
-            print("The inserted file does not exists")
-            exit(-1)
+    def writefile(self, filename, counter):
+        target_row = [filename, str(counter)]
+        self.csv_writer.writerow(target_row)
 
-    def writeFile(self):
-        headers = ["Application","Executors"]
-        targetRow = [self.filename, str(self.extract())]
-        writer = None
-        f = None
-        if not os.path.exists(self.targetDirectory+"/ExecutorCount.csv"):
-            f = open(self.targetDirectory+"/ExecutorCount.csv","w")
-            writer = csv.writer(f, delimiter=',', lineterminator='\n')
-            writer.writerow(headers)
-        else:
-            f = open(self.targetDirectory+"/ExecutorCount.csv","a")
-            writer = csv.writer(f, delimiter=',', lineterminator='\n')
-
-        writer.writerow(targetRow)
-        f.close()
-
-
-    def extract(self):
+    def extract(self, file_):
+        f = open(file_, "r")
         counter = 0
-        for line in self.file:
+        for line in f:
             try:
                 data = json.loads(line)
                 event = data["Event"]
                 if event == "SparkListenerExecutorAdded":
-                    counter=counter + 1
-            except:
+                    counter += 1
+            except :
                 print("Json parsing error")
                 exit(-1)
-        self.file.close()
-        return counter;
+        f.close()
+        return counter
+
+    def create_csv(self):
+        headers = ["Application", "Executors"]
+        self.csv_file = open(self.targetDirectory + "/Executors.csv", "a")
+        self.csv_writer = csv.writer(self.csv_file, delimiter=',', lineterminator='\n')
+        self.csv_writer.writerow(headers)
 
 
 def main():
     args = sys.argv
     if len(args) != 3:
-        print("Required args: [LOG_FILE] [TARGET_DIRECTORY_FOR_RESULTS]")
+        print("Required args: [TOP_DIRECTORY] [TARGET_DIRECTORY_FOR_RESULTS]")
         exit(-1)
     else:
-        extractor = Extractor(str(args[1]),str(args[2]))
+        extractor = Extractor(str(args[1]), str(args[2]))
         extractor.run()
+
 
 if __name__ == "__main__":
     main()
